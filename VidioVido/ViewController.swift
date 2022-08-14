858e1595ce9a252bb93a6a37c275d51c44c7be0c
Muhammad M. Munir <munir@bareksa.com>
2022-08-14 19:45:00 +0700

    Initial Commit

---
+++ VidioVido/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VidioVido
+//
+//  Created by Muhammad M Munir on 14/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
